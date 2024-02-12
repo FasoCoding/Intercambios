1f5c8707a23d81e6fb0dff6cc402b800a954acbf
FasoCoding <bastidas.felipe@google.com>
2024-02-12 15:40:19 -0300

    Ups... falle en el gitignore.

--- old/libs/PRG_connect.py
+++
@@ -1,5 +0,0 @@
-import sqlalchemy as sa
-
-DRIVER = r"{Microsoft Access Driver (*.mdb, *.accdb)}"
-PATH_DB = r"/Antecedentes/Model PRGdia_Full_Definitivo Solution.accdb"
-
--- old/libs/loader.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import sqlalchemy as sa
-
-
-def load_sql_data(sql: str,
-                  cnx: sa.engine) -> pd.DataFrame:
-    
